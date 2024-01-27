58623b20b6499789dc9124fafebfda1ea221c1ce
omerakcali <omerakcali99@gmail.com>
2024-01-27 15:43:50 +0300

    face expressions

--- Assets/_GameAssets/_Scripts/ClientCharacter.cs
+++ Assets/_GameAssets/_Scripts/ClientCharacter.cs
@@ -17,10 +17,49 @@ public class ClientCharacter : MonoBehaviour
     [HorizontalLine(color: EColor.Orange)]
     public ExpressionType DesiredExpressionType;
     public float ExpressionScoreMultiplier;
+    private Dictionary<ExpressionType, ExpressionFace> _faces = new();
+    private ExpressionFace _currentFace;
 
     [Header("Dialogue")] [HorizontalLine(color: EColor.Orange)]
     [ResizableTextArea]
     public string OpeningDialogue;
+
+    private void Awake()
+    {
+        var expressions = GetComponentsInChildren<ExpressionFace>(true);
+
+        foreach (var expression in expressions)
+        {
+            _faces[expression.Type] = expression;
+            if (expression.gameObject.activeSelf) _currentFace = expression;
+        }
+    }
+
+    public void SwitchExpression(ExpressionType type)
+    {
+        if(_currentFace.Type == type) return;
+        _currentFace.gameObject.SetActive(false);
+        _currentFace = _faces[type];
+        _currentFace.gameObject.SetActive(true);
+    }
+
+    [Button()]
+    private void TESTMakeSad()
+    {
+        SwitchExpression(ExpressionType.Negative);
+    }
+    
+    [Button()]
+    private void TESTMakeNeutral()
+    {
+        SwitchExpression(ExpressionType.Neutral);
+    }
+    
+    [Button()]
+    private void TESTMakeHappy()
+    {
+        SwitchExpression(ExpressionType.Positive);
+    }
 }
 
 [Serializable]
